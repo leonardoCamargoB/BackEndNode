fb7eb3150933a1bf6d9da1da301f615c99139836
LEONARDO CAMARGO BENETTE <leonardo.cbenette@senacsp.edu.br>
2025-11-06 16:52:29 -0300

    feat: criado a rota de tarefas.#1

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { Request, Response, NextFunction } from "express";
+import router from "./routes/tarefasRouter";
+
 
 const app = express();
 app.use(express.json());
-
+app.use("/tarefas", router);
 
 
 export default app;
