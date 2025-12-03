4901493d7f6e56eb6549669cf0b3044110496f20
leonardo <74163361+leonardoCamargoB@users.noreply.github.com>
2025-12-03 16:21:59 -0300

    implementado teste da tarefa

---
+++ src/tests/tarefa.test.ts
@@ -0,0 +1,16 @@
+import tarefasRepository from "../repositories/tarefasRepository"
+
+test("GET = /tarefas = 200", async() => {
+  const res = await fetch("http://localhost:3000/tarefas/")
+  expect(res.status).toBe(200)
+
+  const body = await res.json()
+  expect(Array.isArray(body)).toBe(true)
+})
+
+
+
+
+
+
+
