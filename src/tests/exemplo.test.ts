27d7543650297ff9060ec22892542a5d5a891510
leonardo <74163361+leonardoCamargoB@users.noreply.github.com>
2025-12-03 14:33:13 -0300

    feat: criado arquivo para testes

---
+++ src/tests/exemplo.test.ts
@@ -0,0 +1,10 @@
+test("exemplo", ()=>{
+  const nome = "felipe teste da silva"
+  expect(nome).toBe("Felipe Amaral de Souza")
+})
+
+test("exemplo2", ()=>{
+  const result = 5 + 5;
+  expect(result).toBe(10)
+})
+
